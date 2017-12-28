e485a2eb13e250fc2f733c2c15cae16c64de2244
Lassiter Gregg <lassitergregg@gmail.com>
2017-12-27 18:18:02 -0600

    cp5 library

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,24 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-  <section className="album">
-   <h1 className="hero-title">Turn the music up!</h1>
-
-   <section className="selling-points">
-     <div className="point">
-       <h2 className="point-title">Choose your music</h2>
-       <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-     </div>
-     <div className="point">
-       <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-       <p className="point-description">No arbitrary limits. No distractions.</p>
-     </div>
-     <div className="point">
-       <h2 className="point-title">Mobile enabled</h2>
-       <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-     </div>
-   </section>
-  </section>
-);
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
 
 export default Album;
