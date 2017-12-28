dc99ddbb60bca8c9da276fbf44682442cf799dcc
Lassiter Gregg <lassitergregg@gmail.com>
2017-12-27 20:29:46 -0600

    cp8 branch done with extra feature for next

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -38,11 +39,25 @@ class Album extends Component {
       if (this.state.isPlaying && isSameSong) {
         this.pause();
       } else {
-        if (!isSameSong) { this.setSong(song); } 
+        if (!isSameSong) { this.setSong(song); }
         this.play();
       }
     };
-
+    handlePrevClick(){
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play(newSong);
+    }
+    handleNextClick(){
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.min(this.state.album.songs.length, currentIndex + 1);
+      const newSong = this.state.album.songs[newIndex];
+      if(newIndex >= this.state.album.songs.length) {return}
+      this.setSong(newSong);
+      this.play(newSong);
+    }
   render() {
     return (
       <section className="album">
@@ -76,6 +91,13 @@ class Album extends Component {
           )}
           </tbody>
          </table>
+         <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
+         />
       </section>
     );
   }
